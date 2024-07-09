8ebcf1b567e7124933ceb332f484e346beb9c2b3
Ruhul Amin <ruhul.computer12@gmail.com>
2024-07-10 03:23:54 +0600

    JS code added to take inpur from user and click on button to store local storage.

--- Build a Notes Taking Website Using Pure JavaScript/js/app.js
+++ Build a Notes Taking Website Using Pure JavaScript/js/app.js
@@ -1,1 +1,18 @@
 console.log("My Javascript is working.")
+
+let addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', function(e){
+    let addText = document.getElementById('addText') ;
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        noteObj = [];
+    } else {
+        noteObj = JSON.parse(notes);
+    }
+    noteObj.push(addText.value);
+    localStorage.setItem('notes', JSON.stringify(noteObj));
+    addText.value = "";
+    console.log(noteObj);
+
+});

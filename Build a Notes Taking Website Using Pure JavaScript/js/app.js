5dd3e2786589bc907fb61eb9f9943df4c2e43049
Ruhul Amin <ruhul.computer12@gmail.com>
2024-07-10 03:38:23 +0600

    showNotes() Function has been added.

--- Build a Notes Taking Website Using Pure JavaScript/js/app.js
+++ Build a Notes Taking Website Using Pure JavaScript/js/app.js
@@ -15,4 +15,35 @@ addBtn.addEventListener('click', function(e){
     addText.value = "";
     console.log(noteObj);
 
+    showNotes();
 });
+
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null) {
+        noteObj = [];
+    } else {
+        noteObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    noteObj.forEach(function(element, index) {
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem">
+            <div class="card-body">
+                <h5 class="card-title">Note ${index + 1}</h5>
+                <p class="card-text">${element + 1}</p>
+                <button class="btn btn-primary">Delete Note</button>
+            </div>
+        </div>
+`
+    });
+
+    let notesElem = document.getElementById('notes');
+    
+    if(notesElem != 0){
+        notesElem.innerHTML = html;
+    }
+
+}
